7920ce9cc7dd612d0f644b9638c44c1684607778
im-rinnin <im.rinnin@gmail.com>
2024-06-07 20:34:51 +0800

    tree

--- plugin/nvim-tree-setup.lua
+++ plugin/nvim-tree-setup.lua
@@ -1,30 +1,30 @@
 ---- disable netrw at the very start of your init.lua
---vim.g.loaded_netrw = 1
---vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 --
 ---- optionally enable 24-bit colour
---vim.opt.termguicolors = true
+vim.opt.termguicolors = true
 --
 --
----- OR setup with some options
---require("nvim-tree").setup({
---  sort = {
---    sorter = "case_sensitive",
---  },
---   view = { adaptive_size = true },
---  renderer = {
---    group_empty = true,
---  },
---  filters = {
---    dotfiles = false,
---  },
---})
---
---local function open_nvim_tree()
---  require("nvim-tree.api").tree.open()
---end
+-- OR setup with some options
+require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
+   view = { adaptive_size = true },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = false,
+  },
+})
+
+local function open_nvim_tree()
+  require("nvim-tree.api").tree.open()
+end
 ---- alaways open the tree
---vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 --
 --vim.api.nvim_create_autocmd("TabNewEntered", {
 --        callback=open_nvim_tree
