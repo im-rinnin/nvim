4466d5bd9aee7e224ef1107a2dde816a3593e05e
im-rinnin <im.rinnin@gmail.com>
2024-06-07 20:10:18 +0800

    12345

--- lua/plugin-setup.lua
+++ lua/plugin-setup.lua
@@ -8,7 +8,7 @@ require('mason-lspconfig').setup({
 --require("lspconfig").lua-language-server.setup{}
 
 require("symbols-outline").setup({})
-require("nvim-tree").setup({})
+--require("nvim-tree").setup({})
 require("nvim-treesitter").setup(
     {
         ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "css", "python", "rust", "java" },
@@ -29,6 +29,7 @@ require("nvim-treesitter").setup(
 require("nvim-cmp")
 
 require('dap-python').setup('~/.virtualenvs/debugpy/bin/python')
-
+-- init.lua
 local neogit = require('neogit')
 neogit.setup {}
+
